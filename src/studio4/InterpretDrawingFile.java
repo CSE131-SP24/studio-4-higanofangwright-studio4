95bb13e4ea8dd7646a942f5234a26f1b6b58ffd2
connorhigano <connorhigano@localhost>
2024-02-21 15:10:32 -0600

    Did rectangles and ellipses. Triangles stumped us.

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,46 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String shapeType = in.next();
+		
+		int R = in.nextInt();
+		int G = in.nextInt();
+		int B = in.nextInt();
+		
+		boolean filled = in.nextBoolean();
+		
+		double p1 = in.nextDouble();
+		double p2 = in.nextDouble();
+		double p3 = in.nextDouble();
+		double p4 = in.nextDouble();
+		
+		
+		
+		Color color = new Color(R, G, B);
+
+		
+		StdDraw.setPenColor(color);
+		if (shapeType.equals("rectangle")) {
+			if (filled == true) {
+				StdDraw.filledRectangle(p1,p2,p3,p4);
+			}
+			else {
+				StdDraw.rectangle(p1,p2,p3,p4);
+			}
+		
+		}
+		if (shapeType.equals("ellipse")) {
+			if (filled == true) {
+				StdDraw.filledEllipse(p1,p2,p3,p4);
+			}
+			else {
+				StdDraw.ellipse(p1,p2,p3,p4);
+			}
+		
+		}
+		
+		
 		
 	}
 }
